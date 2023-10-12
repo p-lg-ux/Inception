67b83ade5f3cf33edd0e9ff4b2cb3444eb456717
Perrine Gros <pgros@student.42.fr>
2023-10-12 12:24:13 -0400

    finished docker mariadb

---
+++ srcs/requirements/mariadb/tools/db_create.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+service mariadb start;
+
+sleep 1;
+
+mariadb -e "CREATE DATABASE IF NOT EXISTS ${SQL_DATABASE};"
+mariadb -e "CREATE USER IF NOT EXISTS '${SQL_USER}'@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
+mariadb -e "GRANT ALL PRIVILEGES ON ${SQL_DATABASE}.* TO '${SQL_USER}'@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
+
+mariadb -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
+
+mariadb -u root -p${SQL_ROOT_PASSWORD} -e "FLUSH PRIVILEGES;"
+
+mysqladmin -u root --password=${SQL_ROOT_PASSWORD} shutdown
+#mysqladmin -u root shutdown
+
+exec mysqld_safe
---
+++ srcs/requirements/mariadb/tools/export.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+export SQL_DATABASE=my_db
+export SQL_USER=pgros
+export SQL_PASSWORD=1234
+export SQL_ROOT_PASSWORD=groot
